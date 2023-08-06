812c7340c2d5dab2fe5154ef4bfe3c12f2c3fd60
Alice <alyssapolischuck@gmail.com>
2023-08-06 14:25:54 +0300

    fixed minor bug

--- src/components/DarkModeToggle.jsx
+++ src/components/DarkModeToggle.jsx
@@ -6,8 +6,7 @@ import { useEffect } from "react";
 const DarkModeToggle = () => {
   useEffect(() => {
     darkMode(isDarkThemePrefered());
-  }),
-    [];
+  }, [isDarkThemePrefered]);
 
   return (
     <label className="header__dark-mode-toggle">
